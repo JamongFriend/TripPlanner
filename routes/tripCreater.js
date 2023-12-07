ba8389a15afb54e5b3500b8a535909438ad49e4c
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2023-12-07 11:32:59 +0900

    2023.12.07 commit2

--- routes/tripCreater.js
+++ routes/tripCreater.js
@@ -5,26 +5,25 @@ const Plan = require('../models/plan');
 const router = express.Router();
 
 //생성
-router.post(async (req, res) => {
+router.post('/create', async (req, res, next) => {
     const {name, date, peoples, perpose} = req.body;
+    try {
+        await Plan.create({
+            name,
+            date,
+            peoples,
+            perpose
+        });
 
-        try {
-            await Plan.create({
-                name,
-                data,
-                peoples,
-                perpose
-            });
-
-            res.redirect('/');
-        } catch (err) {
-            console.error(err);
-            next(err);
-        }
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 })
 
-//장소 입력
-router.post((req, res) => {
+// 장소 입력
+router.post('/place',(req, res) => {
     const {place} = req.body;
 
     //카카오맵
@@ -35,6 +34,11 @@ router.post((req, res) => {
     };
 
     var map = new kakao.maps.Map(container, options);
+})
+
+// 숙소 입력
+router.post('/hotel',(req, res) => {
+
 })
 
 
