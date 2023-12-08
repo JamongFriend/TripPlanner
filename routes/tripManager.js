bcc861921fb55efc433a254609ed21e49a5214c8
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2023-12-08 10:57:33 +0900

    2023.12.08 commit

--- routes/tripManager.js
+++ routes/tripManager.js
@@ -5,36 +5,68 @@ const { isLoggedIn } = require('./helpers');
 
 const router = express.Router();
 
-//생성한 여행 계획 리스트 확인
-router.get('/readList', (req, res) => {
-    
+//자신이 생성한 여행 계획 리스트 확인
+router.get('/readList', async (req, res, next) => {
+    try {
+        const plan = await Plan.findAll({
+            where: { name: req.params.name },
+            attributes: ['name', 'date', 'peoples', 'perpose', 'place']
+        });
+
+        if (plan) res.json(plan);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 });
-
-
-router.route('/')
-    // 계획 출력
-    .get(isLoggedIn, (req, res) => {
-        res.render('plan', {
-
-        })
-        userId: req.user.id;
-    })
-    // 계획 수정
-    .post(async (req, res, next) => {
-        const userId = req.user.id;
-        if(user){
-            if(name)
-                user.name = name;
-            if(grade)
-                user.grade = grade;
-        }
-        res.send('사용자 정보가 성공적으로 업데이트되었습니다.');
-    })
 
 //여행 계획 출력
-router.get('/readPlan', (req, res) => {
+router.get('/readPlan', async (req, res, next) => {
+    try {
+        const plan = await Plan.findOne({
+            where: { name: req.params.name },
+            attributes: ['name', 'date', 'peoples', 'perpose', 'place']
+        });
 
+        if (plan) res.json(plan);
+        else next(`There is no user with ${req.params.name}.`);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 });
+
+// 여행 계획 수정
+router.post('/update', async (req, res, next) => {
+    try {
+        const result = await Plan.update({
+            description: req.body.description
+        }, {
+            where: { name: req.body.name }
+        });
+
+        if (result) res.redirect('/');
+        else next(`There is no user with ${req.params.name}.`);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+})
+
+// 여행 계획 삭제
+router.get('/delete/:plan', async (req, res, next) => {
+    try {
+        const result = await Plan.destroy({
+            where: { plan: req.params.plan }
+        });
+
+        if (result) next();
+        else next(`There is no user with ${req.params.plan}.`);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+}, logout);
 
 
 module.exports = router;
